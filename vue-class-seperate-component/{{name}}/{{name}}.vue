18f35f5dbc5f0abafbf55606ba499122993e7706
Pekes317 <el_dante565@yahoo.com>
2019-09-14 21:12:20 -0500

    added seperates

---
+++ vue-class-seperate-component/{{name}}/{{name}}.vue
@@ -0,0 +1,5 @@
+<style {{scoped}} src="{{name}}.{{style}}"></style>
+
+<template lang="{{template}}"></template>
+
+<script src="{{name}}.{{script}}"></script>
---
+++ vue-option-sfc/{{name}}/{{name}}.vue
@@ -0,0 +1,16 @@
+<style lang="{{style}}" {{scoped}}>
+</style>
+
+<template lang="{{template}}"></template>
+
+<script lang="{{script}}">
+import Vue from 'vue';
+
+export const {{name}} = Vue.extend({
+    data: () => {
+        return {}
+    }
+});
+
+export default {{name}};
+</script>
